71b62fe15145da9ef5cb336b403762a90de543cd
onb6 <blierolivia@gmail.com>
2021-06-17 13:32:43 -0400

    db credentials

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 app.use(bodyparser.json());
 
 var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'liv',
-    password: '',
-    database: 'proj',
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    user: "b7e2437887xxxa",
+    password: "0200xxx6",
+    database: "heroku_7643ec736354xxx",
     multipleStatements: true
     });
 
